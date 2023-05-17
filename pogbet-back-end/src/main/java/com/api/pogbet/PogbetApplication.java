3266fe70bacb3b6ac2a7818bc8803776a5a9ba25
Henrique Navarro Morais <henriquenavarromorais@gmail.com>
2023-05-17 02:28:30 -0300

    backend commit

---
+++ pogbet-back-end/src/main/java/com/api/pogbet/PogbetApplication.java
@@ -0,0 +1,13 @@
+package com.api.pogbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PogbetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PogbetApplication.class, args);
+	}
+
+}
