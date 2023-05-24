f033b847ece544e23b47be56a3a43ef621ef68ec
Joao-pedro47 <joao.xavier@alunos.ifsuldeminas.edu.br>
2023-05-24 00:29:47 -0300

    Menu Principal, com logo

--- pogbet-front-end/src/Principal.js
+++ pogbet-front-end/src/Principal.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Principal.css'
+import PogBet from "./img/Logo.svg"
 
 class Navbar extends React.Component {
   render() {
     return (
       <nav>
-        < ul className="lista">
+        <diV className="navbar-container">
+           <div className="logo-container">
+            <img src ={PogBet} alt="Logo do site" title="PogBet"/></div>
+        < ul className="menu">
           <li>
           <span className="texto">Jogos</span>
           </li>
@@ -15,7 +19,8 @@ class Navbar extends React.Component {
           <li>
           <span className="texto">Login</span>
           </li>
-        </ul>
+        </ul></diV>
+        
       </nav>
     );
   }
